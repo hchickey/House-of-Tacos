6fb384e3f4298c6cc8db857be1856a0700827958
Haley Hickey <105400433+hchickey@users.noreply.github.com>
2023-02-02 16:10:49 -0600

    small changes

--- src/components/orders/Order.js
+++ src/components/orders/Order.js
@@ -6,8 +6,8 @@ export const Order = ({order, deleteOrder}) => {
     const [orderToppings, setOrderToppings] = useState([])
 
     useEffect( () => {
-        if (order.id) {
-          fetch(`http://localhost:8088/orderToppings?_expand=topping&orderId=${order.id}`)
+        if (order?.id) {
+          fetch(`http://localhost:8088/orders?_expand=topping&orderId=${order.id}`)
           .then( (orderToppingsData) => orderToppingsData.json())
           .then( (oT) => setOrderToppings(oT))
         }
@@ -16,9 +16,9 @@ export const Order = ({order, deleteOrder}) => {
       return (
         <li className="orders__item">
             <p>
-                Order #{order.id} is a {order.tortilla.type} with {order.meat.meat} topped with {orderToppings.map((orderTopping) => orderTopping.topping.topping).join(" and ")}
+                Order #{order?.id} is a {order?.tortilla?.type} with {order?.meat?.meat} topped with {orderToppings.map((orderTopping) => orderTopping?.topping?.topping).join(" and ")}
             </p>
-            <button className="btn--orderDelete" onClick={() => {deleteOrder(order.id)}}>delete</button>
+            <button className="btn--orderDelete" onClick={() => {deleteOrder(order?.id)}}>delete</button>
         </li>
       )
 }
